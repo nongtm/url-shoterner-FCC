880642feffff55571d4002b7f6024347b3db35f6
Tri Nong <minhtri1296@gmail.com>
2016-08-14 03:28:17 +0000

    New version

--- server.js
+++ server.js
@@ -1,37 +1,55 @@
 var express = require('express');
 var app = express();
 
-var date = "";
-var monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
-];
+var MongoClient = require('mongodb').MongoClient;
+var url = process.env.MONGOLAB_URI;
 
-
-app.get('/:date', function (req, res) {
-  var unix = null;
-  var nat = null;
-    var param = req.params.date;
-    if(+param >=0){
-      unix = +param; 
-      date = new Date(+param *1000);
-      nat = monthNames[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear();
-    }
-    
-    else if(isNaN(+param) && new Date(param).toString() !== "Invalid Date"){
-      date = new Date(param);
-      unix = date.valueOf()*1000;
-      nat = monthNames[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear();
-      console.log(date.toString());
-    }
-    
-  var obj = {
-    "unix": unix,
-    "nat" : nat
+MongoClient.connect(url, function(err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
   }
+  else {
+    console.log('Connection established to', url);
+  }
+  
+  var collection = db.collection('url');
+  
+  app.get(/^\/new\/(https?\:\/\/www(\.\w\w+)*)/, function(req, res) {
+    var param = req.params[0];
+    var rand = Math.floor((Math.random()*9000) + 1) + 1000;
+    
+    collection.insert({
+      long: param,
+      short: rand
+    });
+    
+    var obj = {
+      "original_url": param,
+      "short_url": "https://afternoon-lowlands-74617.herokuapp.com/" + rand
+    };
+    res.send(obj);
+  });
+  
+  app.get('/:short', function(req,res){
+    var param = req.params.short;
+    collection.find({
+      short: +param
+    }).toArray(function(err, document){
+      if (err) return err;
+      res.redirect(document[document.length-1].long);
+    });
+  });
+  
+  app.get('/new', function(req,res){
+    res.send("Wrong url format!");
+  });
+  
+  app.get('*',function(req,res){
+    res.send("This url is not on the database!");
+  });
 
-  res.send(obj);
-});
+  app.listen(process.env.PORT || 8080, function() {
+    console.log('Example app listening on port 8080!');
+  });
 
-app.listen(process.env.PORT || 8080, function () {
-  console.log('Example app listening on port 3000!');
 });
